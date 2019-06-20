5265a3e07ca88ac108b7bc6b324755bd71afe365
Prakash Nath Jha <prakash.nath@students.iiit.ac.in>
2019-06-20 23:54:34 +0530

    update

--- venv/src/cfehome/products/forms.py
+++ venv/src/cfehome/products/forms.py
@@ -2,6 +2,20 @@ from django import forms
 
 from .models import Product
 
+class RawProductForm(forms.Form):
+	title = forms.CharField()
+	description = forms.CharField(widget=forms.Textarea())
+	price = forms.DecimalField()
+	summary = forms.CharField()
+	manufacturer = forms.CharField()
+	email = forms.EmailField()
+
+	def clean_email(self,*args,**kwargs):
+		email = self.cleaned_data.get("email")
+		if not email.endswith("edu"):
+			raise forms.ValidationError("Not a valid email")
+		return email
+
 class ProductForm(forms.ModelForm):
 	class Meta:
 		model = Product
--- venv/src/cfehome/products/views.py
+++ venv/src/cfehome/products/views.py
@@ -1,19 +1,63 @@
 from django.shortcuts import render
 from .models import Product
-from .forms import ProductForm
+from .forms import ProductForm, RawProductForm
 # Create your views here.
 
 def product_create_form(request,*args,**kwargs):
-	form = ProductForm(request.POST or None)
 
-	if form.is_valid():
-		form.save()
-		form = ProductForm()
+	my_form = RawProductForm()
+
+	if request.method == "POST":
+		my_form = RawProductForm(request.POST)
+		if my_form.is_valid():
+			print(my_form.cleaned_data)
+			product_obj = Product.objects.create(**my_form.cleaned_data) 
+		else:
+			print(my_form.errors)
 
 	context = {
-		"form" : form
+		"form": my_form
 	}
+
 	return render(request,"create_product.html",context)
+
+# def product_create_form(request,*args,**kwargs):
+	
+# 	product_obj = "No data"
+
+# 	if request.method == "POST":
+# 		title = request.POST.get('title')
+# 		description = request.POST.get('description')
+# 		price = request.POST.get('price')
+# 		summary = request.POST.get('summary')
+# 		manufacturer = request.POST.get('manufacturer')
+
+# 		try:
+# 			if title is not None or description is not None or price is not None or manufacturer is not None or summary is not None:
+# 				product_obj = Product.objects.create(title = title,manufacturer = manufacturer,summary = summary,price = price,description = description)
+# 		except:
+# 			product_obj = None
+
+
+# 	context = {
+# 		"product_obj":product_obj 
+# 	}
+
+# 	return render(request,"create_product.html",context)
+
+# def product_create_form(request,*args,**kwargs):
+# 	form = ProductForm(request.POST or None)
+# 	if form.is_valid():
+# 		form.save()
+# 		form = ProductForm()
+
+# 	context = {
+# 		"form" : form
+# 	}
+
+# 	context = {}
+
+# 	return render(request,"create_product.html",context)
 
 
 def product_detail_view(request,*args,**kwargs):
